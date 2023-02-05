a6dc1fa0f2a2c4a294fa6ef9aae250adea104d92
halisa <halisasaipulaa@gmail.com>
2023-02-05 15:31:54 -0800

    Added Student Class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -13,8 +18,18 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	@GetMapping("/")
-	public String hello(){
-		return "Hello world!";
+	public List<Student> hello(){
+		return List.of(
+				new Student(
+						1L,
+						"Halisa",
+						"lisa@gmail.com",
+						LocalDate.of(2000, Month.APRIL,15),
+						21
+
+
+				)
+		);
 	}
 
 }
