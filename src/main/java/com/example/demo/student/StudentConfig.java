894fc1e98d262fa24e572f78fb382f14bea7a99f
Lisa S <halisasaipulla@gmail.com>
2023-02-05 19:07:33 -0800

    Saving Students

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,35 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student halisa = new Student(
+                    "Halisa",
+                    "lisa@gmail.com",
+                    LocalDate.of(2000, Month.APRIL, 15),
+                    21
+            );
+            Student hena = new Student(
+                    "Hena",
+                    "hena@gmail.com",
+                    LocalDate.of(2005, Month.APRIL, 15),
+                    22
+            );
+
+            repository.saveAll(
+                        List.of(halisa, hena)
+                    );
+        };
+    }
+}
